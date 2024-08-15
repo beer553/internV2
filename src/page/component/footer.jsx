55758ef2a3b4a406d596e153e705692099cad3cf
Beer <sa0935813395@gmail.com>
2024-08-15 10:23:07 +0700

    first commit

---
+++ src/page/component/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div className="bg-black text-white p-3 text-center text-lg mt-10">
+        <p>ติดต่อสอบถาม DX Manpower Management</p>
+        <p className='text-blue-500'>คุณสุพรรษา มูลศิริ supansak@scg.com</p>
+        <p>Digital Transformation Architect (Data Driven-TS)</p>
+        <p>Created by Supansa M and Pantakit S & Developed by Chawanrat B and Phurin C</p>
+        <p>© SCG 2024</p>
+        
+    </div>
+  )
+}
+
+export default footer
---
+++ src/page/intern/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import Footer from '../component/footer';
+
+const home = () => {
+  return (
+    <>
+    <div>
+      <h1>aa</h1>
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default home
